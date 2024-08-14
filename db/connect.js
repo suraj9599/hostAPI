d4ec41dd01c9e8e56f8db2e008da753d8a7820c9
Suraj Singh <110949425+suraj9599@users.noreply.github.com>
2024-08-15 02:38:06 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URL.toString();
 const connectDB = (uri) => {
   console.log("db connected");
   return mongoose.connect(uri);
