aa3df56824be19b18c2fe134a0ce91b559b8a74f
Suraj Singh <s.suraj8595@gmail.com>
2024-08-15 01:51:10 +0530

    host api live

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const nodemon = require("nodemon");
+const app = express();
+const products_routes = require("./routes/products");
+const connectDB = require("./db/connect");
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hi, I'm live");
+});
+
+//middleware or to set router
+app.use("/api/products", products_routes);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => {
+      console.log(`${PORT} Yes I am connected`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ db/connect.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const connectDB = (uri) => {
+  console.log("db connected");
+  return mongoose.connect(uri);
+};
+
+module.exports = connectDB;
